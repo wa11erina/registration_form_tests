d29137c4a688837c8c79bf8aee5dd1051eef041c
Kate Belova <wallerina131@gmail.com>
2023-07-01 21:56:13 +0300

    some improvements in RemoteTestBase made

--- src/test/java/demoqa/tests/RemoteTestBase.java
+++ src/test/java/demoqa/tests/RemoteTestBase.java
@@ -13,7 +13,7 @@ public class RemoteTestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
-        Configuration.remote = "user1:1234@https://selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
