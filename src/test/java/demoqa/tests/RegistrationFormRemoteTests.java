9d555fa749eb052e8d68bf12e76b16e5cb0208fa
Kate Belova <wallerina131@gmail.com>
2023-07-01 20:54:05 +0300

    remote test added

---
+++ src/test/java/demoqa/tests/RegistrationFormRemoteTests.java
@@ -0,0 +1,52 @@
+package demoqa.tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationFormRemoteTests extends TestBase{
+
+    @Test
+    @Tag("remote")
+    void successfulRegistrationTest() {
+        open("https://demoqa.com/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Robert");
+        $("#lastName").setValue("Porter");
+        $("#userEmail").setValue("prot@kpax.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1987");
+        $("div.react-datepicker__day--001:not(.react-datepicker__day--outside-month").click();
+        $("#subjectsInput").setValue("c");
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("prot.jpg");
+        $("#currentAddress").setValue("K-PAX, Lyra constellation");
+        $("#stateCity-wrapper").$("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#stateCity-wrapper").$("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+
+        $(".modal-content").should(appear)
+                .shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Robert Porter"),
+                text("prot@kpax.com"), text("Male"), text("1234567890"),
+                text("01 June,1987"), text("Computer Science"),
+                text("Reading, Music"), text("prot.jpg"),
+                text("K-PAX, Lyra constellation"), text("NCR Delhi"));
+
+    }
+}
